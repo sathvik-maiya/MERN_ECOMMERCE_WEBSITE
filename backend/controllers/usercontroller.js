55f16d07a9f438c1d4a6b0bbafd39409c22588c5
Sathvik V Maiya <94783587+sathvik-maiya@users.noreply.github.com>
2024-08-03 17:51:37 +0530

    added secure and sameSite option for cookies

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -58,6 +58,8 @@ exports.logout = catchasyncerrors(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   });
 
   res.status(200).json({
--- backend/utils/jwttoke.js
+++ backend/utils/jwttoke.js
@@ -10,6 +10,8 @@ const sendtoken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
